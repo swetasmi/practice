5b8d281b72a14ede377f398181b3f981e799efa5
swetasmi <146310377+swetasmi@users.noreply.github.com>
2023-12-15 16:05:07 +0530

    Update BaseClas.java
    branch changed

--- src/main/java/git123/BaseClas.java
+++ src/main/java/git123/BaseClas.java
@@ -9,4 +9,7 @@ public class BaseClas {
 	public void display() {
 		System.out.println("This is a display method");
 	}
+	public void display(String arg) {
+		System.out.println("This is a display method");
+	}
 }
