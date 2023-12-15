671050788080fe034f279d39dd1ce989c82f8cb1
swetasmi <146310377+swetasmi@users.noreply.github.com>
2023-12-15 16:08:23 +0530

    Update BaseClas.java
    deleted main method from soumitra branch

--- src/main/java/git123/BaseClas.java
+++ src/main/java/git123/BaseClas.java
@@ -2,10 +2,7 @@ package git123;
 
 public class BaseClas {
 
-	public static void main(String[] args) {
-		System.out.println("BaseClas");
-		System.out.println("This Is a line from Github");
-	}
+	
 	public void display() {
 		System.out.println("This is a display method");
 	}
